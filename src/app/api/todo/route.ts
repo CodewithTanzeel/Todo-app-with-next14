100794eb3a22b99da6287617281f1885bd28fb9a
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-08-18 09:43:46 +0500

    up

--- src/app/api/todo/route.ts
+++ src/app/api/todo/route.ts
@@ -1,11 +1,14 @@
-// import {db} from '@vercel/postgres'
-import { NextRequest, NextResponse } from 'next/server'
+import { db } from "@vercel/postgres";
+import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(request: NextRequest) {
+  const client = await db.connect();
 
-export function GET(request:NextRequest){
-    return (NextResponse.json({message:"You callled this api"}) 
+  try {
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({message:"Somthing occured"});
+  }
 
-
-);
-
+  return NextResponse.json({ message: "You callled this api" });
 }
