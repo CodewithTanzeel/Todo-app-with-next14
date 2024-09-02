c0b2bc451a72031afd8e9672859874eb7163a9f0
Tanzeel0713 <Tanzeelofficial@outlook.com>
2024-09-02 15:39:05 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Home() {
           className="w-full md:w-[80%] rounded-2xl text-lg px-5 py-2 text-green-950 mb-4 md:mb-0"
         />
         <button
-          className="bg-gray-900 text-white rounded-2xl px-4 py-2 flex items-center hover:bg-zinc-200 hover:text-gray-950"
+          className="bg-gray-900 text-white rounded-2xl ml-4 px-4 py-[1.5px] flex items-center hover:bg-zinc-200 hover:text-gray-950"
           onClick={editMode ? saveEdit : addItem} // Toggle between add and save actions
         >
           {editMode ? "Save" : "ADD TASK"}
